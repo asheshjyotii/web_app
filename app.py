f8619321029a2a7ca99fd37dc9c3f09cef583c1e
ashesh <asheshjyotimajumdar3@gmail.com>
2023-06-29 19:24:43 +0530

    added the flask project

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1> Hello </h1>"
+if __name__ == '__main__':
+
+    app.run(host="0.0.0.0",debug=True)
 
 
 
