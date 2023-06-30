5f64fd5656fd4c5c258f5972585dead7f0e8a91c
ashesh <asheshjyotimajumdar3@gmail.com>
2023-07-01 02:20:01 +0530

    added dynamic data, added layout , buttons etc..

--- app.py
+++ app.py
@@ -2,13 +2,41 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
+JOBS =[
+        {
+            'id': 1,
+            'title' : 'Data Scientist',
+            'location' : 'Bengaluru, India',
+            'salary' : 'Rs. 10,00000'
+        },
+        {
+            'id': 2,
+            'title' : 'Data Analyst',
+            'location' : 'Delhi, India',
+            'salary' : 'Rs. 10,0000'
+        },
+        {
+            'id': 3,
+            'title' : 'ML engineering',
+            'location' : 'Kolkata, India',
+            'salary' : 'Rs. 10,000'
+        },
+        {
+            'id': 4,
+            'title' : 'Front-end Engineer',
+            'location' : 'Kolkata, India',
+            'salary' : 'Rs. 1,000'
+        },
+        {
+            'id': 5,
+            'title' : 'Back-end Engineer',
+            'location' : 'Kolkata, India',
+            'salary' : 'Rs. 100'
+        },
+      ]
 @app.route('/')
 def home():
-    return render_template('home.html')
-
-@app.route('/w3school')
-def w3school():
-    return render_template('w3school.html')
+    return render_template('home.html',job=JOBS)
 
 if __name__ == '__main__':
 
